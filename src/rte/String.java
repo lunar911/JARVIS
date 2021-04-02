84c5f2ea30b1fea27a8e228537dbef955ebc0ff4
Maxim Waganow <maxim.waganow@stud.hs-kempten.de>
2021-04-02 11:40:27 +0200

    split rte.java classes

---
+++ src/rte/String.java
@@ -0,0 +1,16 @@
+package java.lang;
+
+public class String {
+  private char[] value;
+  private int count;
+
+  @SJC.Inline
+  public int length() {
+    return count;
+  }
+
+  @SJC.Inline
+  public char charAt(int i) {
+    return value[i];
+  }
+}
