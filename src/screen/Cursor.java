406d81ad6286585e9b2866ffa703c5766d913289
Maxim Waganow <maxim.waganow@stud.hs-kempten.de>
2021-04-02 11:39:29 +0200

    introduce  pos moving in cursor

--- src/screen/Cursor.java
+++ src/screen/Cursor.java
@@ -4,8 +4,8 @@ public class Cursor {
 
   public int X = 0;
   public int Y = 0;
-  private int width = 80;
-  private int height = 25;
+  private final int width = 80;
+  private final int height = 25;
 
   public void setX(int newX) {
     if (newX <= 80 && newX >= 0) {
@@ -21,5 +21,19 @@ public class Cursor {
 
   public int getPos() {
     return Y * width + X;
+  }
+
+  // handle scrolling in terminal internally
+  public void moveNextPos() {
+    if (X + 1 > width) {
+      X = 0;
+      if (Y + 1 > height) {
+        Y = 0;
+      } else {
+        Y += 1;
+      }
+    } else {
+      X += 1;
+    }
   }
 }
