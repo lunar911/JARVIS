7f0f02b85c067453f23eaf81c9bd0b3ad01411fd
Maxim Waganow <maxim.waganow@stud.hs-kempten.de>
2021-04-03 01:19:31 +0200

    finish print(int)

--- src/screen/Screen.java
+++ src/screen/Screen.java
@@ -39,20 +39,26 @@ public class Screen {
     if (x != 0) {
       int base = 10;
       int digitCount = Math.getDigitCount(x);
-      cursor.setPos(cursor.getPos() + digitCount - 1); // shift cursor right
 
-      int count = 0;
-      while (x > 0) { // print digits right to left
-        char digit = Math.Int2Ascii(x % base);
-        print(digit); // CAUTION: shifts +1 right
-        cursor.setPos(cursor.getPos() - 2); // shift left twice to compensate prev shift
-        x /= base;
-        count++;
-      }
-      if (cursor.getPos() == 0) {
-        cursor.setPos(cursor.getPos() + digitCount);
+      if (digitCount == 1) {
+        print(Math.Int2Ascii(x));
+        x = 0;
       } else {
-        cursor.setPos(cursor.getPos() + digitCount + 1);
+        cursor.setPos(cursor.getPos() + digitCount - 1); // shift cursor right
+
+        int count = 0;
+        while (x > 0) { // print digits right to left
+          char digit = Math.Int2Ascii(x % base);
+          print(digit); // CAUTION: shifts +1 right
+          cursor.setPos(cursor.getPos() - 2); // shift left twice to compensate prev shift
+          x /= base;
+          count++;
+        }
+        if (cursor.getPos() == 0) { // workaround for a issue I'm not really getting..
+          cursor.setPos(cursor.getPos() + digitCount);
+        } else {
+          cursor.setPos(cursor.getPos() + digitCount + 1);
+        }
       }
     } else {
       print('0');
