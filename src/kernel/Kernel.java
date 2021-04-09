d553cd45116c04ebf0dc2a4f0d48200bd2a7292c
Maxim Waganow <maxim.waganow@stud.hs-kempten.de>
2021-04-09 23:16:23 +0200

    add realmode transitions

--- src/kernel/Kernel.java
+++ src/kernel/Kernel.java
@@ -1,17 +1,43 @@
 package kernel;
 
-import screen.*;
 import interrupt.*;
+import rte.*;
+import screen.*;
 
 public class Kernel {
 
   public static void main() {
+    Screen screen = new Screen();
     Screen.clearScreen();
 
     InterruptHandler.initPic();
-    
-    MAGIC.inline(0xCD, 0x00); // trigger divideError interrupt
 
-    while (true);
+    BIOS.regs.EAX = 0x0013;
+    BIOS.rint(0x10);
+
+    for (int i = 0; i < 320 * 66; i++) MAGIC.wMem32(0xA0000 + i, (int) 0x00);
+
+    for (int i = 320 * 66; i < 320 * 133; i++) MAGIC.wMem32(
+      0xA0000 + i,
+      (int) 0x0C
+    );
+
+    for (int i = 320 * 133; i < 320 * 200; i++) MAGIC.wMem32(
+      0xA0000 + i,
+      (int) 0x0E
+    );
+
+    MAGIC.wIOs8(0x70, (byte) 0);
+    byte seconds = (byte) (MAGIC.rIOs8(0x71) & 0xFF);
+    byte ts = seconds;
+    while(seconds + 20 > ts){
+      MAGIC.wIOs8(0x70, (byte) 0);
+      ts = (byte) (MAGIC.rIOs8(0x71) & 0xFF);    
+    }
+
+    BIOS.regs.EAX = 0x0003;
+    BIOS.rint(0x10);
+
+    while(true);
   }
 }
