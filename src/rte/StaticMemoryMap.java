702d5f611724704e4e3c97af3de3feaf9ac629d0
Maxim Waganow <natkia@hotmail.de>
2021-05-11 11:23:05 +0200

    Add StaticMemoryMap

---
+++ src/rte/StaticMemoryMap.java
@@ -0,0 +1,32 @@
+package rte;
+
+import bios.BIOS;
+
+public class StaticMemoryMap {
+    public static long startAddress = 0;
+    public static long size = 0;
+    public static int type = 0;
+
+
+    private static final int buffer = 0x8000;
+
+    public static boolean next() {
+        if (BIOS.regs.EAX != 0x534D4150 || BIOS.regs.EBX == 0) return false;
+        BIOS.regs.EBX = 0;
+
+        BIOS.regs.EAX = 0x0000E820;
+        BIOS.regs.EDX = 0x534D4150; // SMAP
+
+        // store result in
+        BIOS.regs.EDI = buffer;
+        // buffer size >= 20 bytes
+        BIOS.regs.ECX = 24;
+
+        BIOS.rint(0x15);
+
+        startAddress = MAGIC.rMem64(buffer);
+        size = MAGIC.rMem64(buffer + 8) - startAddress;
+        type = MAGIC.rMem32(buffer + 16);
+        return true;
+    }
+}
