0f61326e2ed7a7b5551c4e0e3e6a256daba4e865
Maxim Waganow <natkia@hotmail.de>
2021-05-13 12:30:28 +0200

    intermediate DynamicRuntime

--- src/rte/DynamicRuntime.java
+++ src/rte/DynamicRuntime.java
@@ -1,21 +1,30 @@
 package rte;
 
+import screen.Screen;
+import bios.BIOS;
+
 public class DynamicRuntime {
 
-    private static int nextFreeAddress = 0;
     private static int firstFreeAddress = 0;
-
     private static EmptyObject first_eO = null;
 
     public static void InitEmptyObjects() {
+        BIOS.regs.EBX = 0;
+
         int imageSize = MAGIC.rMem32(MAGIC.imageBase + 4);
         firstFreeAddress = (MAGIC.imageBase + imageSize + 0xFFF) & ~0xFFF;
 
         while (StaticMemoryMap.next()) {
             if (StaticMemoryMap.type == 1) {// Memory is free.
-                if (StaticMemoryMap.startAddress >= firstFreeAddress) {
-                    int eO_address = (int) StaticMemoryMap.startAddress;
-                    int eO_size = (int) StaticMemoryMap.size & ~3; // align size to multiple of 4, in doubt decrease size
+                if (StaticMemoryMap.startAddress + StaticMemoryMap.size >= firstFreeAddress) {
+                    int eO_address, eO_size;
+                    if(StaticMemoryMap.startAddress >= firstFreeAddress) { // Take memory segment from BIOS as is.
+                        eO_address = (int) StaticMemoryMap.startAddress;
+                        eO_size = (int) StaticMemoryMap.size;
+                    } else { // Shrink memory segment
+                        eO_address = (int) firstFreeAddress;
+                        eO_size = (int) StaticMemoryMap.size - (firstFreeAddress - eO_address);
+                    }
 
                     // erase data at empty object area
                     for (int i = eO_address; i < eO_address + eO_size; i++) {
@@ -27,20 +36,15 @@ public class DynamicRuntime {
                     MAGIC.assign(eO._r_type, MAGIC.clssDesc("EmptyObject"));
                     MAGIC.assign(eO._r_scalarSize, eO_size);
 
-                    if (first_eO == null) { // create First empty object
+                    if (first_eO == null) {
                         first_eO = (EmptyObject) eO;
-                        //MAGIC.assign(eO._r_next, null); Assign _r_next on second object
                     } else {
-                        if(first_eO.nextFreeAddress == null) { // Second empty object
-                            first_eO.nextFreeAddress = (EmptyObject) eO;
-                            MAGIC.assign(eO._r_next, eO);
-                        }
-                        // Any other empty object
                         EmptyObject iter = first_eO;
                         // get last EmptyObject
-                        while(iter.nextFreeAddress != null) {
-                            iter = iter.nextFreeAddress;
+                        while (iter.nextEmptyObject != null) {
+                            iter = iter.nextEmptyObject;
                         }
+                        iter.nextEmptyObject = (EmptyObject) eO;
                         MAGIC.assign(iter._r_next, eO);
                     }
                 }
@@ -53,81 +57,153 @@ public class DynamicRuntime {
             int relocEntries,
             SClassDesc type
     ) {
-        if (nextFreeAddress == 0) {
-            int imageSize = MAGIC.rMem32(MAGIC.imageBase + 4);
-            firstFreeAddress = (MAGIC.imageBase + imageSize + 0xFFF) & ~0xFFF;
-            nextFreeAddress = firstFreeAddress;
+        if (first_eO == null) {
+            InitEmptyObjects();
+        }
+        scalarSize = (scalarSize + 3) & ~3; // Align to multiples of 4
+
+
+        //find next big enough EmptyObject
+        EmptyObject eO = first_eO;
+        while ((eO._r_scalarSize - 8) < scalarSize) {
+            eO = eO.nextEmptyObject;
+            if(eO == null) {
+                MAGIC.inline(0xCC);
+            }
         }
 
+        int start = MAGIC.cast2Ref(eO) + eO._r_scalarSize - scalarSize; // Start of current new object
         int rs = relocEntries << 2;
-        scalarSize = (scalarSize + 3) & ~3; // Align to multiples of 4
-        int start = nextFreeAddress; // Start of current new object
-        nextFreeAddress += rs + scalarSize; // offset for next new object
+        int freeAddress = start + rs + scalarSize; // offset for next new object
 
-        for (int i = start; i < nextFreeAddress; i += 4) {
+        for (int i = start; i < freeAddress; i += 4) {
             MAGIC.wMem32(i, 0); // init memory for object with 0
         }
 
-        Object newObj = MAGIC.cast2Obj(start + rs); // offset object by its references
+        Object newObj = MAGIC.cast2Obj(start);
         MAGIC.assign(newObj._r_relocEntries, relocEntries);
         MAGIC.assign(newObj._r_scalarSize, scalarSize);
         MAGIC.assign(newObj._r_type, type);
-        MAGIC.assign(newObj._r_next, MAGIC.cast2Obj(nextFreeAddress));
 
+        MAGIC.assign(newObj._r_next, eO._r_next);
+
+        MAGIC.assign(eO._r_scalarSize, eO._r_scalarSize - scalarSize); // shrink emptyObject by size of new object
         return newObj;
     }
 
-    public static SArray newArray(
-            int length,
-            int arrDim,
-            int entrySize,
-            int stdType,
-            Object unitType
-    ) {
-        while (true) ;
+    public static SArray newArray(int length, int arrDim, int entrySize, int stdType,
+                                  SClassDesc unitType) { //unitType is not for sure of type SClassDesc
+        int scS, rlE;
+        SArray me;
+
+        if (stdType == 0 && unitType._r_type != MAGIC.clssDesc("SClassDesc"))
+            MAGIC.inline(0xCC); //check type of unitType, we don't support interface arrays
+        scS = MAGIC.getInstScalarSize("SArray");
+        rlE = MAGIC.getInstRelocEntries("SArray");
+        if (arrDim > 1 || entrySize < 0) rlE += length;
+        else scS += length * entrySize;
+        me = (SArray) newInstance(scS, rlE, MAGIC.clssDesc("SArray"));
+        MAGIC.assign(me.length, length);
+        MAGIC.assign(me._r_dim, arrDim);
+        MAGIC.assign(me._r_stdType, stdType);
+        MAGIC.assign(me._r_unitType, unitType);
+        return me;
     }
 
-    public static void newMultArray(
-            SArray[] parent,
-            int curLevel,
-            int destLevel,
-            int length,
-            int arrDim,
-            int entrySize,
-            int stdType,
-            Object unitType
-    ) {
-        while (true) ;
+    public static void newMultArray(SArray[] parent, int curLevel, int destLevel,
+                                    int length, int arrDim, int entrySize, int stdType, SClassDesc clssType) {
+        int i;
+
+        if (curLevel + 1 < destLevel) { //step down one level
+            curLevel++;
+            for (i = 0; i < parent.length; i++) {
+                newMultArray((SArray[]) ((Object) parent[i]), curLevel, destLevel,
+                        length, arrDim, entrySize, stdType, clssType);
+            }
+        } else { //create the new entries
+            destLevel = arrDim - curLevel;
+            for (i = 0; i < parent.length; i++) {
+                parent[i] = newArray(length, destLevel, entrySize, stdType, clssType);
+            }
+        }
     }
 
     public static boolean isInstance(Object o, SClassDesc dest, boolean asCast) {
-        while (true) ;
+        SClassDesc check;
+
+        if (o == null) {
+            if (asCast) return true; //null matches all
+            return false; //null is not an instance
+        }
+        check = o._r_type;
+        while (check != null) {
+            if (check == dest) return true;
+            check = check.parent;
+        }
+        if (asCast) MAGIC.inline(0xCC);
+        return false;
     }
 
-    public static SIntfMap isImplementation(
-            Object o,
-            SIntfDesc dest,
-            boolean asCast
-    ) {
-        while (true) ;
+    public static SIntfMap isImplementation(Object o, SIntfDesc dest, boolean asCast) {
+        SIntfMap check;
+
+        if (o == null) return null;
+        check = o._r_type.implementations;
+        while (check != null) {
+            if (check.owner == dest) return check;
+            check = check.next;
+        }
+        if (asCast) MAGIC.inline(0xCC);
+        return null;
     }
 
-    public static boolean isArray(
-            SArray o,
-            int stdType,
-            Object unitType,
-            int arrDim,
-            boolean asCast
-    ) {
-        while (true) ;
+    public static boolean isArray(SArray o, int stdType, SClassDesc clssType, int arrDim, boolean asCast) {
+        SClassDesc clss;
+
+        //in fact o is of type "Object", _r_type has to be checked below - but this check is faster than "instanceof" and conversion
+        if (o == null) {
+            if (asCast) return true; //null matches all
+            return false; //null is not an instance
+        }
+        if (o._r_type != MAGIC.clssDesc("SArray")) { //will never match independently of arrDim
+            if (asCast) MAGIC.inline(0xCC);
+            return false;
+        }
+        if (clssType == MAGIC.clssDesc("SArray")) { //special test for arrays
+            if (o._r_unitType == MAGIC.clssDesc("SArray"))
+                arrDim--; //an array of SArrays, make next test to ">=" instead of ">"
+            if (o._r_dim > arrDim) return true; //at least one level has to be left to have an object of type SArray
+            if (asCast) MAGIC.inline(0xCC);
+            return false;
+        }
+        //no specials, check arrDim and check for standard type
+        if (o._r_stdType != stdType || o._r_dim < arrDim) { //check standard types and array dimension
+            if (asCast) MAGIC.inline(0xCC);
+            return false;
+        }
+        if (stdType != 0) {
+            if (o._r_dim == arrDim) return true; //array of standard-type matching
+            if (asCast) MAGIC.inline(0xCC);
+            return false;
+        }
+        //array of objects, make deep-check for class type (PicOS does not support interface arrays)
+        if (o._r_unitType._r_type != MAGIC.clssDesc("SClassDesc")) MAGIC.inline(0xCC);
+        clss = o._r_unitType;
+        while (clss != null) {
+            if (clss == clssType) return true;
+            clss = clss.parent;
+        }
+        if (asCast) MAGIC.inline(0xCC);
+        return false;
     }
 
-    public static void checkArrayStore(Object dest, SArray newEntry) {
-        while (true) ;
+    public static void checkArrayStore(SArray dest, SArray newEntry) {
+        if (dest._r_dim > 1) isArray(newEntry, dest._r_stdType, dest._r_unitType, dest._r_dim - 1, true);
+        else if (dest._r_unitType == null) MAGIC.inline(0xCC);
+        else isInstance(newEntry, dest._r_unitType, true);
     }
 
     public static void nullException() {
-        //vielleicht noch was ausgeben...
-        while (true) ;
+        MAGIC.inline(0xCC);
     }
 }
