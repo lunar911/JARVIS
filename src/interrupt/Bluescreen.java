1737c46716fc421d007d76996879794b06b178b2
Maxim Waganow <natkia@hotmail.de>
2021-05-31 22:13:11 +0200

    Apply fixes for functioning Bluescreen.

--- src/interrupt/Bluescreen.java
+++ src/interrupt/Bluescreen.java
@@ -14,29 +14,32 @@ public class Bluescreen {
         Screen.directPrintString("EBP", 0, 0, 0x07);
         Screen.directPrintInt(ebp, 16, 0, 1, 0x07);
 
-        int eip = MAGIC.rMem32(ebp + 4);
+        int eip = MAGIC.rMem32(ebp + 4*9);
         Screen.directPrintString("EIP", 8, 0, 0x07);
         Screen.directPrintInt(eip, 16, 8, 1, 0x07);
 
+        Screen.directPrintString("Class", 20, 0, 0x07);
+        Screen.directPrintString("Method", 40, 0, 0x07);
+
         final int stackBegin = 0x9BFFC;
 
-        int count = 1;
-        int nextEBP = ebp;
-        while (nextEBP < stackBegin && nextEBP > 0) {
-            int nextEIP = MAGIC.rMem32(nextEBP + 4);
+        int lineCount = 1;
+        while (ebp < stackBegin && ebp > 0) {
 
-            Screen.directPrintInt(nextEBP, 16, 0, count, 0x07);
-            Screen.directPrintInt(nextEIP, 16, 8, count, 0x07);
+            Screen.directPrintInt(ebp, 16, 0, lineCount, 0x07);
+            Screen.directPrintInt(eip, 16, 8, lineCount, 0x07);
             SMthdBlock methodBlock = getMethodName(eip);
 
             if(methodBlock == null) {
-                Screen.directPrintString("no Method found.", 20, count, 0x07);
+                Screen.directPrintString("no Method found.", 20, lineCount, 0x07);
             } else {
-                Screen.directPrintString(methodBlock.namePar, 20, count, 0x07);
+                Screen.directPrintString(methodBlock.owner.name, 20, lineCount, 0x07);
+                Screen.directPrintString(methodBlock.namePar, 40, lineCount, 0x07);
             }
 
-            nextEBP = MAGIC.rMem32(nextEBP);
-            count++;
+            ebp = MAGIC.rMem32(ebp);
+            eip = MAGIC.rMem32(ebp + 4);
+            lineCount++;
         }
         while (true) ;
     }
@@ -48,7 +51,7 @@ public class Bluescreen {
         SMthdBlock method = null;
 
         while(p != null) {
-            StaticV24.println(p.name);
+            //StaticV24.println(p.name);
             method = loopClasses(eip, p.units);
             if(method != null)
                 break;
@@ -66,8 +69,8 @@ public class Bluescreen {
     private static SMthdBlock loopClasses(int eip, SClassDesc c) {
         SMthdBlock method = null;
         while(c != null) {
-            StaticV24.print("/");
-            StaticV24.println(c.name);
+            //StaticV24.print("/");
+            //StaticV24.println(c.name);
             method = loopMethods(eip, c.mthds);
             if(method != null)
                 break;
@@ -80,10 +83,10 @@ public class Bluescreen {
 
     private static SMthdBlock loopMethods(int eip, SMthdBlock m) {
         while(m != null) {
-            StaticV24.print("/-/");
-            StaticV24.print(m.namePar);
-            for(int i = m.namePar.length(); i < 60; i++)
-                StaticV24.print(' ');
+            //StaticV24.print("/-/");
+            //StaticV24.print(m.namePar);
+            //for(int i = m.namePar.length(); i < 60; i++)
+            //    StaticV24.print(' ');
             if(inRange(eip, m))
                 break;
             m = m.nextMthd;
@@ -95,14 +98,14 @@ public class Bluescreen {
     private static boolean inRange(int eip, SMthdBlock m) {
         int start = MAGIC.cast2Ref(m);
         int end = start + m._r_scalarSize;
-        StaticV24.print("0x");
+        /*StaticV24.print("0x");
         StaticV24.printHex(start,8);
         StaticV24.print(" <= 0x");
         StaticV24.printHex(eip, 8);
         StaticV24.print(" < 0x");
         StaticV24.printHex(end, 8);
         StaticV24.print(" = ");
-        StaticV24.println((start <= eip && eip < end) ? "true\n\n\n\n\n\n" : "f");
+        StaticV24.println((start <= eip && eip < end) ? "true\n\n\n\n\n\n" : "f");*/
         return start <= eip && eip < end;
     }
 }
