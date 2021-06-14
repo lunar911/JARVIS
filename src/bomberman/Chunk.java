85be6e8618d7ccf32cad48dade715d98822eb609
Maxim Waganow <natkia@hotmail.de>
2021-06-14 10:46:07 +0200

    fix coordinate issue

--- src/bomberman/Chunk.java
+++ src/bomberman/Chunk.java
@@ -5,17 +5,16 @@ import peripheral.StaticV24;
 public class Chunk {
     private byte color = 0;
     private int pos = 0; // upper left corner of chunk
-    private final int chunkSize = 20;
+    private static final int chunkSize = 20;
 
     public Chunk(int pos) {
         this.pos = pos;
     }
 
     public void setColor(byte color) {
-        StaticV24.println(pos);
-        for(int i = 0; i < chunkSize; i++) {
-            for(int j = 0; j < chunkSize; j++) {
-                MAGIC.wMem8(0xA0000 + pos + i * 320 + j, (byte) color);
+        for(int y = 0; y < chunkSize; y++) { // write col
+            for(int x = 0; x < chunkSize; x++) { // write row
+                MAGIC.wMem8(0xA0000 + pos + y * 320 + x, (byte) color);
             }
         }
     }
--- src/bomberman/Game.java
+++ src/bomberman/Game.java
@@ -1,10 +1,12 @@
 package bomberman;
 
 import bios.BIOS;
+import peripheral.StaticV24;
 
 public class Game {
     private static final int chunkSize = 20;
-    private static final int pxInRow = chunkSize * chunkSize * 15;
+    private static final int chunksPerRow = 320 / chunkSize;
+    private static final int pxInRow = chunkSize * chunkSize * 16;
     private Chunk[] chunks;
 
     public Game() {
@@ -13,8 +15,19 @@ public class Game {
         BIOS.enterGraphicmode();
         for(int i = 0; i < chunks.length; i++){
             byte color = i % 2 == 0 ? (byte) 1 : (byte) 2;
-            chunks[i] = new Chunk(i * chunkSize + (i / 16 * pxInRow));
-            chunks[i].setColor(color);
+            int pos = (i * chunkSize) % (chunksPerRow * chunkSize) + (i / 16 * pxInRow);
+
+            /*
+            StaticV24.print(i);
+            StaticV24.print(" ");
+            StaticV24.print(pxInRow);
+            StaticV24.print(" ");
+            StaticV24.println(pos);
+            */
+
+            chunks[i] = new Chunk(pos);
+            chunks[i].setColor((byte) i);
+
         }
     }
 }
